0acdef59ad7ad0b4ecf06cf16ff2919863978a3a
d4r6 <h112358132134@korea.ac.kr>
2020-08-27 23:43:25 +0900

    Add "Create" & "Store"

--- Array/Array.c
+++ Array/Array.c
@@ -1,11 +1,20 @@
 #include "Array.h"
 
-Array::Array() :test(1)
-{}
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
 
-Array::~Array()
-{}
+void Create(Array* array, Long capacity, size_t size)
+{
+	array->front = calloc(capacity, size);
+	array->capacity = capacity;
+	array->length = 0;
+}
 
-void Array::Create(int capacity){
-
+void Store(Array* array, Long index, void* object, size_t size)
+{
+	// sizeof(void) : unknown size.
+	memcpy((char*)(array->front) + (index * size), object, size);
+	array->length++;
+	return index;
 }
--- Array/Array.h
+++ Array/Array.h
@@ -4,10 +4,14 @@
 #include <stddef.h>
 
 typedef signed long int Long;
+
 typedef struct _array {
 	void(*front);
 	Long capacity;
 	Long length;
 }Array;
+
+void Create(Array* array, Long capacity, size_t size);
+void Store(Array* array, Long index, void* object, size_t size);
 
 #endif _ARRAY_H_
